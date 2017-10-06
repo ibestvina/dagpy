7b730512de315475b1af9de9a361e5b8c5999f38
Ivan Bestvina <ivan.bestvina@fer.hr>
2017-10-06 18:32:05 +0200

    DAG executor proof of concept

--- src/dag.py
+++ src/dag.py
@@ -31,10 +31,12 @@ class DAG_executor:
 
     def _run_ipynb_command(self, block_id):
         filename = utils.block_filename(self._dag.id, block_id)
-        return os.path.join(self._blocks_path, filename)
+        filepath = os.path.join(self._blocks_path, filename)
+        return 'jupyter nbconvert --to notebook --execute ' + filepath
 
     def _execute_block(self, block_id):
         command = self._run_ipynb_command(block_id)
+        print(command)
         self._cex.run(command, block_id)
 
     def execute_blocks(self, block_ids):
@@ -42,6 +44,9 @@ class DAG_executor:
         pending_parents_cnt = {block_id: len(self._dag.parents_of(block_id)) for block_id in pending_to_execute}
 
         while pending_to_execute or self._cex.run_cnt():
+            print('pending', pending_to_execute)
+            print('parent_cnt', pending_parents_cnt)
+            print('running', self._cex.run_cnt())
             execute_now = [block_id for block_id in pending_to_execute if not pending_parents_cnt[block_id]]
             for block_id in execute_now:
                 pending_to_execute.remove(block_id)
@@ -50,6 +55,7 @@ class DAG_executor:
             if self._cex.run_cnt():
                 retval = self._cex.blocking_poll(0.1)
                 if retval['retcode'] != 0:
-                    print('Error executing block', block_id)
+                    print('Error executing block', retval['proc_id'])
                     print('Execution output:\n', retval['output'].read())
-                pending_parents_cnt[retval.proc_id] -= 1
+                for child_id in self._dag.children_of(retval['proc_id']):
+                    pending_parents_cnt[child_id] -= 1
