ebc79fe16a3d6c28b07ff7dba0e39a82eee18ad5
Ivan Bestvina <ivan.bestvina@fer.hr>
2017-10-08 21:12:57 +0200

    Program.py edit

--- src/program.py
+++ src/program.py
@@ -5,14 +5,14 @@ import flowmanager
 import argparse
 
 
-# dag_filepath = 'playground/dag_example_1.txt'
+dag_filepath = 'playground/dag_example_1.txt'
 #
-# mydag = dag.DAG.from_file(dag_filepath)
+mydag = dag.DAG.from_file(dag_filepath)
 # print(mydag._dag_dict)
 # #print(utils.linearize_dependencies(mydag, ['B']))
 #
-# #mydagex = dagexecutor.DAGExecutor(mydag, '')
-# #mydagex.execute_blocks(['D'])
+mydagex = dagexecutor.DAGExecutor(mydag, '')
+mydagex.execute_blocks(['D'])
 #
 # #myflowmng = flowmanager.FlowManager(mydag, '')
 # #nb = myflowmng.flow_to_file(['D'], 'merged.ipynb')
