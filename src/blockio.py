b473769376b424c36c219ac0217e2cb8542a0586
Ivan Bestvina <10189206+ibestvina@users.noreply.github.com>
2017-10-16 15:02:20 +0200

    Update blockio.py

--- src/blockio.py
+++ src/blockio.py
@@ -4,23 +4,27 @@ import nbformat
 from nbformat.v4 import new_notebook, new_code_cell
 
 
-TITLE_CELL_TYPE = 'title'
-DELIMITER_CELL_TYPE = 'delimiter'
-INPUT_FILTER_CELL_TYPE = 'input_filter'
-OUTPUT_FILTER_CELL_TYPE = 'output_filter'
-HEADER_CELL_TYPE = 'header'
+TITLE_CELL_TYPE = 'title'  # flow title cell, markdown, not parsed
+DELIMITER_CELL_TYPE = 'delimiter'  # flow delimiter cell, markdown, parsed for block attributes
+INPUT_FILTER_CELL_TYPE = 'input_filter'  # input filter cell, code, generated for filter input functionality
+OUTPUT_FILTER_CELL_TYPE = 'output_filter'  # output filter cell, code, generated for filter output functionality
+HEADER_CELL_TYPE = 'header'  # header cell, code, generated for various dagpy flow functionalities (e.g. start new block)
 
 
 
 def filter_file(var_name, block_id, dag):
+    """Return path to the filter (.dill) file."""
+    # TODO: give users the freedom to choose where and by which name filtered vars is saved.
     return 'cache/{}_{}.dill'.format(str(block_id), str(var_name))
     #return os.path.join('cache', fname)
 
 def placeholder_cell():
+    """Create a new code cell with placeholder comment. Used in empty blocks."""
     new_code_cell(source='# Hi, this is an empty DAGpy block. Add your cells here!\n')
 
 
 def filter_input_cell(block_id, dag):
+    """Input filter cell code. Imports dill and loads all of the parents' filtered vars."""
     source_str = '# DAGpy input filter\nimport dill'
     parents = dag.parents_of(block_id)
     for parent in parents:
@@ -32,6 +36,7 @@ def filter_input_cell(block_id, dag):
     return new_code_cell(source=source_str, metadata={'dagpy':{'cell_type': INPUT_FILTER_CELL_TYPE}})
 
 def filter_output_cell(block_id, dag):
+    """Ouput filter cell code. Imports dill and dumps all of the block's output filter vars."""
     source_str = '# DAGpy output filter\n# To change the filter, edit the block properties, not this cell.\nimport dill\nimport os'
     block_filter = dag.get_block_att(block_id, 'filter', default = [])
     for var_name in block_filter:
@@ -42,6 +47,7 @@ def filter_output_cell(block_id, dag):
 
 
 def save_block(block_id, block_cells, dag):
+    """Save the block to the ipynb block file"""
     nb = new_notebook()
     nb.metadata['dagpy'] = {}
     nb.metadata['dagpy']['block_id'] = block_id
