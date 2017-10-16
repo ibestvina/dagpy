ed71174f9bc21215609d9de92590bda39182de5a
Ivan Bestvina <10189206+ibestvina@users.noreply.github.com>
2017-10-16 15:29:04 +0200

    Update dagexecutor.py

--- src/dagexecutor.py
+++ src/dagexecutor.py
@@ -3,24 +3,31 @@ import os.path
 
 
 class DAGExecutor:
+    """Manages the DAG blocks execution and Jupyter kernel communication."""
 
     def __init__(self, dag, path):
+        """Init from DAG object used for block lookup and .dagpy path to which block files are relative."""
         self._dag = dag
         self._path = os.path.dirname(path)
         self._cex = utils.ConsoleExecutor()
 
     def _run_ipynb_command(self, block_id):
+        """Command which runs and updates (inplace) a iPython notebook."""
+        # TODO: replace with the iPython API calls.
         #filename = utils.block_filename(self._dag.get_id(), block_id)
         fname = self._dag.block_filename(block_id)
         fpathname = os.path.join(self._path, fname)
         return 'jupyter nbconvert --ExecutePreprocessor.timeout=-1 --execute --inplace ' + fpathname
 
     def _execute_block(self, block_id):
+        """Execute a block."""
         command = self._run_ipynb_command(block_id)
         print('Executing', block_id)
         self._cex.run(command, block_id)
 
     def execute_blocks(self, block_ids):
+        """Execute multiple blocks and all their dependencies."""
+        # TODO: check for changes and execute only what needs updating
         pending_to_execute = utils.all_dependencies(self._dag, block_ids)
         pending_parents_cnt = {block_id: len(self._dag.parents_of(block_id)) for block_id in pending_to_execute}
         block_cnt = len(pending_to_execute)
