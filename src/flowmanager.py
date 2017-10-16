d090b147e7c437517eca1202cc82e6f88ec9d7d6
Ivan Bestvina <10189206+ibestvina@users.noreply.github.com>
2017-10-16 15:37:33 +0200

    Update flowmanager.py

--- src/flowmanager.py
+++ src/flowmanager.py
@@ -9,26 +9,34 @@ import nbformat
 from nbformat.v4 import new_markdown_cell, new_code_cell, new_notebook
 
 class FlowManager:
+    """Manages the creation, parsing and saving of flow notebooks""" 
 
     def __init__(self, dag, path):
+        """Init from DAG object used for block lookup and .dagpy path to which block files are relative."""
         self._dag = dag
         self._path = path
 
     @staticmethod
     def read_nb(fpathname):
+        """Read the iPython notebook file."""
         with open(fpathname, encoding='utf-8') as f:
             return nbformat.read(f, as_version=4)
 
     def _read_block_nb(self, block_id):
+        """Read the block notebook file."""
         fname = self._dag.block_filename(block_id)
         fpathname = os.path.join(self._path, fname)
         return self.read_nb(fpathname)
 
     def _header_cell(self):
+        """Create the header cell with custom DAGpy code, such as the start_new_dagpy_block function.
+        Header cell is loaded from the header_cell.py file.
+        """
         source = open("header_cell.py").read()
         return new_code_cell(source = source, metadata = {'dagpy': {'cell_type': blockio.HEADER_CELL_TYPE}})
 
     def _delimiter_cell(self, block_id):
+        """Delimiter cell between blocks, specifying the following block metadata."""
         source = '### Block_id\n' + str(block_id) + '\n'
         source += '##### Description\n' + self._dag.get_block_att(block_id, 'description', default='') + '\n'
         source += '##### Parents\n' + ', '.join(self._dag.get_block_att(block_id, 'parents', default='')) + '\n'
@@ -37,10 +45,12 @@ class FlowManager:
         return new_markdown_cell(source=source, metadata={'dagpy': {'cell_type': blockio.DELIMITER_CELL_TYPE, 'block_id': block_id}})
 
     def _create_notebook(self):
+        """Create new flow notebook with title and header cells"""
         title_cell = new_markdown_cell(source='## DAGpy Flow notebook\nIf you want to start a new block, just run the start_new_dagpy_block() in the cell!', metadata={'dagpy': {'cell_type': blockio.TITLE_CELL_TYPE}})
         return new_notebook(cells=[self._header_cell(), title_cell])
 
     def merge_blocks(self, block_ids):
+        """Merge blocks into a flow notebook."""
         merged = self._create_notebook()
         for block_id in block_ids:
             nb = self._read_block_nb(block_id)
@@ -50,6 +60,7 @@ class FlowManager:
         return merged
 
     def flow_to_file(self, block_ids, filepathname):
+        """Create flow and save it to file."""
         block_ids_linearized = utils.linearize_dependencies(self._dag, block_ids)
         nb = self.merge_blocks(block_ids_linearized)
         nbformat.write(nb, filepathname)
@@ -57,6 +68,7 @@ class FlowManager:
 
     @staticmethod
     def parse_delimiter_cell_att(att_name, lines):
+        """Parse a single block attribute from a delimiter cell."""
         if att_name == 'block_id':
             return re.sub( '\s+', '', ''.join(lines))
         if att_name == 'description':
@@ -69,6 +81,7 @@ class FlowManager:
 
     @staticmethod
     def parse_delimiter_cell(cell):
+        """Parse a delimiter cell to retrieve block metadata attributes."""
         block_meta = {}
         lines = cell.source.splitlines()
         att_name = None
@@ -103,6 +116,7 @@ class FlowManager:
 
 
     def apply_flow_changes(self, filepathname):
+        """Apply changes made in the flow to block files and the DAG."""
         cells = self.read_nb(filepathname).cells
         blocks_to_save = {}
         block_cells = []
